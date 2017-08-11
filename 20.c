040913eec7908159a9562a3d6692dd81158e2023
sivarajamms <30342749+sivarajamms@users.noreply.github.com>
2017-08-11 19:23:26 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int n, i;
-
-    printf("Enter an integer: ");
+    int m, i;
+     printf("Enter an integer: ");
     scanf("%d",&n);
 
     for(i=1; i<=10; ++i)
     {
-        printf("%d * %d = %d \n", n, i, n*i);
+        printf("%d * %d = %d \n", m, i, n*i);
     }
     
     return 0;
